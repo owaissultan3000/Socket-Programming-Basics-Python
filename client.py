6723cb6bafc8942caa0d06b7a017dfa170e1ee02
owaissultan3000 <noman.anjum@liberatrade.ai>
2021-09-14 19:28:23 +0500

    updated

--- client.py
+++ client.py
@@ -1,7 +1,50 @@
 import socket
 import uuid
 import hashlib
-from cryptography.fernet import Fernet
+
+
+# The Encryption Function
+def cipher_encrypt(plain_text, key):
+
+    encrypted = ""
+
+    for c in plain_text:
+
+        if c.isupper():  #check if it's an uppercase character
+
+            c_index = ord(c) - ord('A')
+
+            # shift the current character by key positions
+            c_shifted = (c_index + key) % 26 + ord('A')
+
+            c_new = chr(c_shifted)
+
+            encrypted += c_new
+
+        elif c.islower():  #check if its a lowecase character
+
+            # subtract the unicode of 'a' to get index in [0-25) range
+            c_index = ord(c) - ord('a')
+
+            c_shifted = (c_index + key) % 26 + ord('a')
+
+            c_new = chr(c_shifted)
+
+            encrypted += c_new
+
+        elif c.isdigit():
+
+            # if it's a number,shift its actual value
+            c_new = (int(c) + key) % 10
+
+            encrypted += str(c_new)
+
+        else:
+
+            # if its neither alphabetical nor a number, just leave it like that
+            encrypted += c
+
+    return encrypted
 
 
 def encrypt_string(message, key):
@@ -9,17 +52,18 @@ def encrypt_string(message, key):
     salt = uuid.uuid4().hex
     merged = str(salt) + message
     encrypted_string = ""
-    fernet = Fernet(key)
+    
 
-    encrypted_string = fernet.encrypt(merged.encode()).decode()
+    encrypted_string = cipher_encrypt(merged, key)
 
     hash_string = hashlib.sha256(merged.encode()).hexdigest()
 
-    return fernet, hash_string, encrypted_string
+    return hash_string, encrypted_string
 
 message = "my name is owais"
-key = Fernet.generate_key()
-(fernet, hash_string, encrypt_string) = encrypt_string(message, key)
+key = 3
+(hash_string, encrypt_string) = encrypt_string(message, key)
+print(hash_string, encrypt_string)
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv.bind(('0.0.0.0', 8080))
@@ -32,8 +76,8 @@ while True:
         if not data: break
         from_client += data
         print("from_client", from_client)
-        conn.send(fernet)
-        conn.send(hash_string)
+        # conn.send(key)
+        # conn.send(hash_string)
         conn.send(encrypt_string)
     conn.close()
     print ('client disconnected')
--- server.py
+++ server.py
@@ -2,9 +2,52 @@ import socket
 import hashlib
 
 
-def decrypt_string(hash_string, fernet, encrypt_string):
+# The Decryption Function
+def cipher_decrypt(ciphertext, key):
 
-    decrypted_string_server = fernet.decrypt(encrypt_string.encode()).decode()
+    decrypted = ""
+
+    for c in ciphertext:
+
+        if c.isupper():
+
+            c_index = ord(c) - ord('A')
+
+            # shift the current character to left by key positions to get its original position
+            c_og_pos = (c_index - key) % 26 + ord('A')
+
+            c_og = chr(c_og_pos)
+
+            decrypted += c_og
+
+        elif c.islower():
+
+            c_index = ord(c) - ord('a')
+
+            c_og_pos = (c_index - key) % 26 + ord('a')
+
+            c_og = chr(c_og_pos)
+
+            decrypted += c_og
+
+        elif c.isdigit():
+
+            # if it's a number,shift its actual value
+            c_og = (int(c) - key) % 10
+
+            decrypted += str(c_og)
+
+        else:
+
+            # if its neither alphabetical nor a number, just leave it like that
+            decrypted += c
+
+    return decrypted
+
+
+def decrypt_string(hash_string, key, encrypt_string):
+
+    decrypted_string_server = cipher_decrypt(encrypt_string, key)
 
 
     hash_string_server = hashlib.sha256(
@@ -19,13 +62,14 @@ def decrypt_string(hash_string, fernet, encrypt_string):
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('0.0.0.0', 8080))
 
-fernet = client.recv(4096)
-hash_string = client.recv(4096)
+# key = client.recv(4096)
+# hash_string = client.recv(4096)
 encrypt_string = client.recv(4096)
+print(encrypt_string)
 
-result = decrypt_string(hash_string, fernet, encrypt_string)
-if result:
-    client.send("Data Receive Without Manuplation")
-else:
-    client.send("Data Receive With Manuplation")
+# result = decrypt_string(hash_string, key, encrypt_string)
+# if result:
+#     client.send("Data Receive Without Manuplation")
+# else:
+#     client.send("Data Receive With Manuplation")
 client.close()
